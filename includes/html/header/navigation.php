19688daa91c5905d54d5670264f63c762ce36c7f
gregory_sf9 <greg@studioforty9.com>
2018-06-12 17:48:49 +0100

    Added category links from DB

--- includes/html/header/navigation.php
+++ includes/html/header/navigation.php
@@ -1,3 +1,6 @@
+<?php
+    require 'includes/db.php';
+?>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -13,15 +16,21 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
+            <?php
+            // make global $connection as the variable is in different file
+            global $connection;
+            //prepare SQL query to retrieve all data from categories table
+            $query = "SELECT cat_title FROM categories";
+            // get data from categories table
+            $selectCategories = mysqli_query($connection, $query);
+
+            //create links
+            while($row = mysqli_fetch_assoc($selectCategories)): ?>
+               <li>
+                   <a href="#"><?php echo $row['cat_title'];?></a>
+               </li>
+            <?php endwhile; ?>
+            ?>
             </ul>
         </div>
         <!-- /.navbar-collapse -->
